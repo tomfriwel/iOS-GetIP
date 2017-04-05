5050436fceee6e23c5cc6e0a7bff09f92a6a35c5
tomfriwel <976932447@qq.com>
2017-04-05 22:50:23 +0800

    Initial Commit

---
+++ GetIP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetIP
+//
+//  Created by tomfriwel on 05/04/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetIP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetIP
+//
+//  Created by tomfriwel on 05/04/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
